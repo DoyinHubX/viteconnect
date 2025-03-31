b0d1a7c5cd2ea2f1372c1cf79e2b9086567cad02
Doyin <doyinelugbadebo@gmail.com>
2025-03-31 20:15:36 +0100

    complete crud operations

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Contact
+from .forms import ContactForm
 
-# Create your views here.
+# Home - List Contacts
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
+
+# Create Contact
+def add_contact(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactForm()
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+# Update Contact
+def edit_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+# Delete Contact
+def delete_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == "POST":
+        contact.delete()
+        return redirect('contact_list')
+    return render(request, 'contacts/contact_confirm_delete.html', {'contact': contact})
